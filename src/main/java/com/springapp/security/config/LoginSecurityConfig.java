37398255174880440dc8421cdecd465bed7cc3b3
dungGrooovy <mayonesama12@gmail.com>
2016-06-02 15:08:28 +0700

    first

---
+++ src/main/java/com/springapp/security/config/LoginSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.springapp.security.config;
+
+/**
+ * Created by quangdung on 02/06/2016.
+ */
+public class LoginSecurityConfig {
+}
---
+++ target/Logindemo/WEB-INF/views/welcomePage.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1>You are in Welcome page</h1>
+</body>
+</html>
